448dbf44c74fefb4c08642d536145dceb7e2f31b
Anamika-1629 <anamikarai835@gmail.com>
2026-05-24 18:11:17 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/dev/anamika/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anamika.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
