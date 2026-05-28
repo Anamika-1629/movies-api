e64b1364421cc8aaa8b283f8a353665336f51154
Anamika-1629 <anamikarai835@gmail.com>
2026-05-28 23:45:42 +0530

    Refactor project structure and finalize backend setup

---
+++ src/main/java/dev/anamika/movies/controllers/ReviewController.java
@@ -0,0 +1,28 @@
+package dev.anamika.movies.controllers;
+
+import dev.anamika.movies.repositories.ReviewService;
+import dev.anamika.movies.models.Reviews;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping //handles POST requests
+    //@RequestBody --> takes JSON data send by the user and converts it to Java data
+    //reviewService.createReview() --> methods calls the Service class to create the review and connect it to the certain movie in movie collection
+    private ResponseEntity<Reviews> createReview(@RequestBody Map<String, String> payload){
+        return new ResponseEntity<Reviews>(reviewService.createReview(payload.get("reviewBody"),payload.get("imdbId")), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/dev/anamika/movies/repositories/ReviewService.java
@@ -0,0 +1,39 @@
+package dev.anamika.movies.repositories;
+
+import dev.anamika.movies.models.Movies;
+import dev.anamika.movies.models.Reviews;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    //Repos are used for basic CRUD operations but with templates, we can perform advance operations directly on the db
+    //here, insert review --> update movie --> push review into movie's review list
+    //we can use the template to perform dynamic query and do the operations inside our db without using the repo
+    private MongoTemplate mongoTemplate;
+
+
+    public Reviews createReview(String reviewBody, String imdbId){
+        // Creates a new review document with fields: id and body
+        Reviews review = reviewRepository.insert(new Reviews(reviewBody));
+
+        //update the document inside the Movies collection
+        mongoTemplate.update(Movies.class)
+                // Find the movie whose imdbId matches the imdbId received from the user
+                .matching(Criteria.where("imdbId").is(imdbId))
+
+                // Update the movie document by adding the newly created review to the reviewIds array in the movies collection
+                .apply(new Update().push("reviewIds").value(review.getId()))
+                .first();
+
+        return review;
+    }
+}
