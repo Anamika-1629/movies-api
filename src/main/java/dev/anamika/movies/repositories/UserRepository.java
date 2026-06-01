181e48c6727ce5e434da0175cbcafc7aa8b0a03c
Anamika-1629 <anamikarai835@gmail.com>
2026-06-01 14:13:08 +0530

    Add Spring Security and JWT dependencies

---
+++ src/main/java/dev/anamika/movies/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package dev.anamika.movies.repositories;
+
+import dev.anamika.movies.models.Users;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<Users, ObjectId> {
+    Optional <Users> findByEmail(String email);
+}
